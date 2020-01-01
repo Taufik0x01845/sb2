0e235ca60e21896cd1b6d4080bedbc5848613858
ehunaj <56645834+ehunaj@users.noreply.github.com>
2020-01-01 12:53:16 +0800

    Update config.py

--- linepy/config.py
+++ linepy/config.py
@@ -27,8 +27,8 @@ class Config(object):
         'LINE_SERVICES': '1459630796'
     }
 
-    APP_TYPE    = 'IOSIPAD\x097.18.1\x09iPhone_OS\x0910.12.0' #CHROMEOS\t1.4.17\tChrome_OS\t1' #ApplicationType._VALUES_TO_NAMES[368]
-    #APP_VER     = '2.1.5'
+    APP_TYPE    = ApplicationType.IOS
+    #APP_VER     = '7.14.0'
     CARRIER     = '51089, 1-0'
     SYSTEM_NAME = 'Ehun'
     #SYSTEM_VER  = '11.2.5'
@@ -36,6 +36,6 @@ class Config(object):
     EMAIL_REGEX = re.compile(r"[^@]+@[^@]+\.[^@]+")
 
     def __init__(self):
-        self.APP_NAME = 'IOSIPAD\x097.18.1\x09iPhone_OS\x0910.12.0' #CHROMEOS\t1.4.17\tChrome_OS\t1' #s\t%s\t%s\t%s' % (self.APP_TYPE, self.APP_VER, self.SYSTEM_NAME, self.SYSTEM_VER)
-        self.USER_AGENT = 'Line/7.18.1'' #1.4.17' #s' % self.APP_VER
+        self.APP_NAME = 'IOSIPAD\x097.14.0\x09iPhone_OS\x0911.2.5' #CHROMEOS\t1.4.17\tChrome_OS\t1' #s\t%s\t%s\t%s' % (self.APP_TYPE, self.APP_VER, self.SYSTEM_NAME, self.SYSTEM_VER)
+        self.USER_AGENT = 'Line/7.14.0'' #1.4.17' #s' % self.APP_VER
  
